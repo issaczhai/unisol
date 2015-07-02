5f2c1f0bf48771a3bad4b3ac3f9f65799810fdb1
FENG XIN <jackyfeng1218@gmail.com>
2015-07-02 16:32:16 +0800

    Change to Feng Xin working version
    with type and year list

--- DMX/Manager/ProjectManager.php
+++ DMX/Manager/ProjectManager.php
@@ -83,6 +83,38 @@ class ProjectManager {
         return $totalNo;
     }
     
+    function getTypeList(){
+        $type_arr = array();
+        $ConnectionMgr = new ConnectionManager();
+        $conn = $ConnectionMgr->getConnection();
+        $stmt = $conn->prepare("SELECT DISTINCT type FROM project");
+        $stmt->execute();
+        $stmt->bind_result($type);
+        while ($stmt->fetch())
+        {   
+            $project_type = $type;
+            array_push($type_arr,$project_type);
+        }
+        $ConnectionMgr->closeConnection($stmt, $conn);
+        return $type_arr;
+    }
+	
+	function getYearList(){
+        $year_arr = array();
+        $ConnectionMgr = new ConnectionManager();
+        $conn = $ConnectionMgr->getConnection();
+        $stmt = $conn->prepare("SELECT DISTINCT year FROM project ORDER BY year DESC");
+        $stmt->execute();
+        $stmt->bind_result($year);
+        while ($stmt->fetch())
+        {   
+            $project_year = $year;
+            array_push($year_arr,$project_year);
+        }
+        $ConnectionMgr->closeConnection($stmt, $conn);
+        return $year_arr;
+    }
+    
     function getPaginatedResults($pageNo, $itemPerPage){
         $project_arr = array();
         $ConnectionMgr = new ConnectionManager();
@@ -166,37 +198,5 @@ class ProjectManager {
         return $projectArr;
     }
     
-	
-	function getTypeList(){
-        $type_arr = array();
-        $ConnectionMgr = new ConnectionManager();
-        $conn = $ConnectionMgr->getConnection();
-        $stmt = $conn->prepare("SELECT DISTINCT type FROM project");
-        $stmt->execute();
-        $stmt->bind_result($type);
-        while ($stmt->fetch())
-        {   
-            $project_type = $type;
-            array_push($type_arr,$project_type);
-        }
-        $ConnectionMgr->closeConnection($stmt, $conn);
-        return $type_arr;
-    }
-	
-	function getYearList(){
-        $year_arr = array();
-        $ConnectionMgr = new ConnectionManager();
-        $conn = $ConnectionMgr->getConnection();
-        $stmt = $conn->prepare("SELECT DISTINCT year FROM project ORDER BY year DESC");
-        $stmt->execute();
-        $stmt->bind_result($year);
-        while ($stmt->fetch())
-        {   
-            $project_year = $year;
-            array_push($year_arr,$project_year);
-        }
-        $ConnectionMgr->closeConnection($stmt, $conn);
-        return $year_arr;
-    }
     
 }
