34d440782f8922d5136fbf34fdbdac55f4843f9b
FENG XIN <xin.feng.2012@sis.smu.edu.sg>
2015-12-29 22:40:40 +0800

    Implement Course Detail Framework

---
+++ PingTanConsultant/course_detail.php
@@ -0,0 +1,159 @@
+<!DOCTYPE html>
+<html>
+
+<head>	
+	<meta name="keywords" content="PingTan, safety school, construction">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="./public_html/css/style.css">
+</head>
+<body>
+	<!-- Header -->
+	<?php
+        include_once("./templates/header.php");
+    ?>
+
+    <!-- Content -->
+	    <div class="container-fluid course-detail-content">
+	    	<div class="row row-course-detail">
+	    		<div class="col-md-5 col-md-offset-1 course-detail-img">
+	    			<h3 class="title-session yellow-default">Work at Height for Assessors</h3>
+	    			<img src="./public_html/img/course-detail.png">
+	    		</div>
+	    		<div class="col-md-5 course-detail-info">
+	    			<div class="row course-detail-info-row">
+	    				<div class="col-md-1">
+	    					<i class="fa fa-language fa-lg"></i>
+	    				</div>
+	    				<div class="col-md-3">
+	    					<span>Language:</span>
+	    				</div>
+	    				<div class="col-md-8">
+	    					<span>English</span>
+	    				</div>
+	    			</div>
+	    			<div class="row course-detail-info-row">
+	    				<div class="col-md-1">
+	    					<i class="fa fa-map-marker fa-lg"></i>
+	    				</div>
+	    				<div class="col-md-3">
+	    					<span>Location:</span>
+	    				</div>
+	    				<div class="col-md-8">
+	    					<span>8 New Industrial Road LHK 3 Building #06-04 Singapore 536200</span>
+	    				</div>
+	    			</div>
+	    			<div class="row course-detail-info-row">
+	    				<div class="col-md-1">
+	    					<i class="fa fa-calendar fa-lg"></i>
+	    				</div>
+	    				<div class="col-md-3">
+	    					<span>Course Type:</span>
+	    				</div>
+	    				<div class="col-md-8">
+	    					<select name="course-type">
+	    						<option value="full-time">Full-Time</option>
+	    						<option value="part-time">Part-Time</option>
+	    					</select>
+	    				</div>
+	    			</div>
+	    			<div class="row course-detail-info-row">
+	    				<div class="col-md-1">
+	    					<i class="fa fa-clock-o fa-lg"></i>
+	    				</div>
+	    				<div class="col-md-3 title-dateTime">
+	    					<span>Date and Time:</span>
+	    				</div>
+	    				<div class="col-md-8 dateTime">
+	    					<span>Monday - Friday, 9AM - 6PM</span>
+	    				</div>
+	    			</div>
+	    			<div class="row course-detail-info-row">
+	    				<div class="col-md-1">
+	    					<i class="fa fa-hourglass-half fa-lg"></i>
+	    				</div>
+	    				<div class="col-md-3 titled-duration">
+	    					<span>Total Hours:</span>
+	    				</div>
+	    				<div class="col-md-8 duration">
+	    					<span>40 Hours</span>
+	    				</div>
+	    			</div>
+	    			<div class="row course-detail-info-row">
+	    				<div class="col-md-1">
+	    					<i class="fa fa-calendar-check-o fa-lg"></i>
+	    				</div>
+	    				<div class="col-md-3">
+	    					<span>Start Date:</span>
+	    				</div>
+	    				<div class="col-md-8">
+	    					<select name="start-date">
+	    						<option value="01012016">1 Jan 2016</option>
+	    						<option value="01082016">8 Jan 2016</option>
+	    					</select>
+	    				</div>
+	    			</div>
+	    			<div class="row course-detail-info-row">
+	    				<div class="col-md-1">
+	    					<i class="fa fa-usd fa-lg"></i>
+	    				</div>
+	    				<div class="col-md-3 titled-price">
+	    					<span>Price:</span>
+	    				</div>
+	    				<div class="col-md-8 price">
+	    					<span class="red-default"> SGD 100 (GST Inclusive)</span>
+	    				</div>
+	    			</div>
+	    			<div class="row course-detail-info-row">
+	    				<div class="col-md-12">
+			    			<button class="btn-enrollment">Enroll me for this course</button>		
+						</div>
+	    			</div>
+	    		</div>
+	    	</div>
+
+	    	<div class="row">
+	    		<div class="col-md-6 col-md-offset-1 course-info">
+	    			<h3>Course Information</h3>
+	    			<h4>Course Summary</h4>
+	    			<p>
+	    				Work at Height for Assessors course is to provide the participants with the requisite work at height knowledge to assess work at height activities so that work is carried out properly and safely. It is a requirement that participants must have completed and passed the WAH supervisor course.
+	    			</p>
+	    			<h4>Course Content</h4>
+	    			<ul class="list-course-content">
+	    				<li>Identify, explain and apply the relevant Workplace Safety and Health (WSH) Legislations / Approved Code of Practice / Singapore Standards applicable to Work-at-Heights (WAH)
+	    				</li>
+	    				<li>Identify and explain the roles & responsibilities of a WAH Assessor
+	    				</li>
+	    				<li>Identify hazards, evaluate the risk, and implement control measures for WAH activities
+	    				</li>
+	    				<li>Evaluate the adequacy and effectiveness of the Fall Prevention Plan</li>
+	    				<li>Evaluate whether the control measures implemented on site for PTW are adequate and effect
+	    				</li>
+	    			</ul>
+	    			<h4>Why This Course ?</h4>
+	    			<p>Upon successful completion of the course and passing the examination, a certificate of successful completion endorsed by PingTan Consultant Pte Ltd will be issued to the participants.
+	    			</p>
+	    			<h4>Certification</h4>
+	    			<p>Upon successful completion of the course and passing the examination, a certificate of successful completion endorsed by PingTan Consultant Pte Ltd will be issued to the participants.
+	    			</p>
+	    		</div>
+	    		<div class="col-md-4 course-prerequisite">
+	    			<h3>Prerequisites</h3>
+	    			<h5 class="prerequisite"><a class="yellow-default" href="#">Work at Height for Assessors</a></h5>
+	    			<h5 class="prerequisite"><a class="yellow-default" href="#">Work at Height for Assessors</a></h5>
+	    		</div>
+	    	</div>
+	    </div>
+
+
+    <!-- Footer -->
+    <?php
+        include_once("./templates/footer.php");
+    ?>
+
+<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+</body>
+
+</html>
