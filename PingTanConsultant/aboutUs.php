4c024f414b893cdf516023e2f982f2a761f96e30
FENG XIN <xin.feng.2012@sis.smu.edu.sg>
2015-12-29 12:34:54 +0800

    Implement AboutUs
    1. Team section is not implemented

---
+++ PingTanConsultant/aboutUs.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html>
+
+<head>	
+	<meta name="keywords" content="PingTan, safety school, construction">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="./public_html/css/style.css">
+
+    <script src="http://maps.googleapis.com/maps/api/js"></script>
+    <?php
+        //$address = $dlocation; // Google HQ
+        $prepAddr = str_replace(' ','+','8 New Industrial Road LHK 3 Building Singapore 536200');
+        $geocode = file_get_contents('http://maps.google.com/maps/api/geocode/json?address='.$prepAddr.'&sensor=false');
+        $output = json_decode($geocode);
+        $latitude = $output->results[0]->geometry->location->lat;
+        $longitude = $output->results[0]->geometry->location->lng;
+    ?>
+    <!-- Map Script -->
+	<script>
+	    function initialize() {
+	        var myLatlng = new google.maps.LatLng(<?= $latitude?>,<?= $longitude?>);
+	        var mapProp = {
+	          center:myLatlng,
+	          zoom:18,
+	          mapTypeId:google.maps.MapTypeId.ROADMAP
+	        };
+	        var map=new google.maps.Map(document.getElementById("map_canvas"),mapProp);
+	        var marker = new google.maps.Marker({
+	            position: myLatlng,
+	            map: map,
+	            title: 'Ping Tan Construction'
+	        });
+	    }
+	    google.maps.event.addDomListener(window, 'load', initialize);
+    </script>
+</head>
+<body>
+	<!-- Header -->
+	<?php
+        include_once("./templates/header.php");
+    ?>
+
+    <!-- Content -->
+	    <div class="container-fluid container-fluid-padding-none">
+	    	<div class="row row-title-about">
+	    		<h3>About PingTan Construction</h2>
+	    	</div>
+	    	<!-- Content -->
+	    	<div class="row content-about"> 
+	    		<div class="col-md-7 col-md-offset-1 about-left-content">
+	    			<div class="about-banner">
+	    				<img src="./public_html/img/aboutUsBanner.png">
+	    			</div>
+	    			<!-- <h3 class="red-default">About Our Company</h3> -->
+	    			<p>To enable the sustainability of our strategy and, in turn the achievement of our vision, we must be clear not only about what we will do, but also how we will do it. These are behaviours that we expect from all our employees under all circumstances. We call these non-negotiable behaviours and have defined as core principles and values:
+	    			</p>
+	    			<h4 class="red-default">Core Principles</h4>
+	    			<h5>Quality Management</h5>
+	    			<p>We are committed to creating long-term value for our clients, business partners and employees. The drive is towards providing high-quality products and services at a competitive price. This disciplined, customer-focused approach is fundamental to building successful partnerships with our clients to drive growth, sustainability and profitability.
+	    			</p>
+	    			<h5>Rish Prevention</h5>
+	    			<p>Our wealth of expertise and industry knowledge provides the opportunity to constabtly seek innovative ways to abide by the most stringent safety measures. We bring together this expertise throughout our business to allow our operation to remain responsive to the dynamics of the business in which we operate in.
+	    			</p>
+	    			<h4 class="red-default">Values</h4>
+	    			<h5>Team Work</h5>
+	    			<p>Central to our organization is the developmental growth of our employees as a team. Every employee deserves to work in an attractive environment, in safe and healthy conditions. We are committed to diversity in the work place where there is mutual trust and respect, and an equal opportunity for every employee to develop personally and professionally as each enhances their skills and capabilities.
+	    			</p>
+	    			<h5>Integrity</h5>
+	    			<p>We insist on integrity and fairness in all our business dealings to maintain the highest level of ethical standards. Conditioning our operations with integrity and with respect for our clients, business partners and employees will always remain at the heart of our corporate responsibility. 
+	    			</p>
+	    			<h5>Commitment</h5>
+	    			<p>Delivering on commitments - we do what we say we will do. Our keen passion for excellence is targeted to deliver the best in everything we do every day for our clients. This is where we build endearing relationships and leverage our personal strengths to win together as a team with every client. 
+	    			</p>
+	    		</div>
+
+	    		<div class="col-md-3 about-right-content">
+	    			<h3>LATEST NEWS</h3>
+	    			<ul class="list-about">
+	    				<li>
+	    					<h5 class="news-date">10.11.2015</h5>
+	    					<h5 class="red-default text-weight-mid">Our website is lunached</h5>
+	    					<p class="news-content">In 2015, we finally lunached our PingTan consultant website. You can apply safety courses and make payment directly from our website. It will also help you to track your study progress and documents. The certificate will be available for download once you pass the exam.</p>
+	    				</li>
+	    				<li>
+	    					<h5 class="news-date">10.11.2015</h5>
+	    					<h5 class="red-default text-weight-mid">Our website is lunached</h5>
+	    					<p class="news-content">In 2015, we finally lunached our PingTan consultant website. You can apply safety courses and make payment directly from our website. It will also help you to track your study progress and documents. The certificate will be available for download once you pass the exam.</p>
+	    				</li>
+	    				<li>
+	    					<h5 class="news-date">10.11.2015</h5>
+	    					<h5 class="red-default text-weight-mid">Our website is lunached</h5>
+	    					<p class="news-content">In 2015, we finally lunached our PingTan consultant website. You can apply safety courses and make payment directly from our website. It will also help you to track your study progress and documents. The certificate will be available for download once you pass the exam.</p>
+	    				</li>
+	    			</ul>
+	    		</div>
+	    	</div>
+	    </div>
+    <!-- Footer -->
+    <?php
+        include_once("./templates/footer.php");
+    ?>
+
+<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+</body>
+
+</html>
